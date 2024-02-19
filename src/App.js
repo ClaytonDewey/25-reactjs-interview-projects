b42b0f2fcd0ed2e4c2cfcaa6551b87b2ee36d4a9
ClaytonDewey <clay@dryadmedia.com>
2024-02-19 16:46:27 -0600

    Finished image slider component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 // import Accordion from './components/accordion';
 // import RandomColor from './components/random-color';
+// import StarRating from './components/star-rating';
 
-import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
-    <div
-      className='App'
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
+    <div>
       {/* Accordion component */}
       {/* <Accordion /> */}
       {/* Random Color component */}
       {/* <RandomColor /> */}
       {/* Star Rating Component */}
-      <StarRating noOfStars={10} />
+      {/* <StarRating noOfStars={10} /> */}
+      {/* Image Slider  */}
+      <ImageSlider
+        url={'https://picsum.photos/v2/list'}
+        page={'1'}
+        limit={'10'}
+      />
     </div>
   );
 }
