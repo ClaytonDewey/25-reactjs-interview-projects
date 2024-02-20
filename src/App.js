c1a2b6a65abf6ad0032054aed3a71653be1abb2b
ClaytonDewey <clay@dryadmedia.com>
2024-02-20 16:02:18 -0600

    Start Scroll Indicator component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@
 // import TreeView from './components/tree-view';
 // import menus from './components/tree-view/data';
 // import QRCodeGenerator from './components/qr-code-generator';
+// import LightDarkMode from './components/light-dark-mode';
 
-import LightDarkMode from './components/light-dark-mode';
+import ScrollIndicator from './components/scroll-indicator';
 
 function App() {
   return (
@@ -26,7 +27,9 @@ function App() {
       {/* <TreeView menus={menus} /> */}
       {/* QR Code Generator */}
       {/* <QRCodeGenerator /> */}
-      <LightDarkMode />
+      {/* <LightDarkMode /> */}
+      {/* Scroll Indicator */}
+      <ScrollIndicator url={'https://dummyjson.com/products?limit=100'} />
     </div>
   );
 }
